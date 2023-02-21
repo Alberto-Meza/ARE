6527bd640f2cc41dd0a64725bf4905bfdb7e9ca2
Alberto-Meza <alberto_k1@hotmail.com>
2023-02-21 12:04:53 -0700

    Create Structure
    Comit initial

---
+++ ARE/ARE.Mobile/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace ARE.Mobile;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
+
---
+++ ARE/ARE.Mobile/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace ARE.Mobile;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
+
