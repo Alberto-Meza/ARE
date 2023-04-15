2554b2e06f146af72e1aa468bd88c75c5e00dfb3
Alberto-Meza <alberto_k1@hotmail.com>
2023-04-14 22:33:03 -0700

    comit Standar CRUD

---
+++ ARE/ARE.WEB/Helpers/HelperActions.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace ARE.WEB.Helpers
+{
+	public class HelperActions
+	{
+		public Action Action { get; set; }
+		public Action<Dictionary<string,object>> ActionByParams { get; set; }
+	}
+}
+
